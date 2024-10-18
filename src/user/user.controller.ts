2a69fc3892f2a1f6b4e55e35b0cd974df5f61cf1
Orest Kozolup <o.kozolup.extern@instamotion.com>
2024-10-18 14:50:08 +0200

    setup jwt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,13 +2,16 @@ import { Controller, Post, Body } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/createUser.dto";
 import { UserEntity } from "./user.entity";
+import { UserResponseInterface } from "./types/userResponse.interface";
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserEntity> {
-    return await this.userService.createUser(createUserDto);
+  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserDto);
+
+    return this.userService.buildUserResponse(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,9 @@ import { CreateUserDto } from "./dto/createUser.dto";
 import { UserEntity } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { sign } from "jsonwebtoken";
+import { JWT_SECRET } from "@app/config";
+import { UserResponseInterface } from "./types/userResponse.interface";
 
 @Injectable()
 export class UserService {
@@ -15,5 +18,22 @@ export class UserService {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user)
+      }
+    }
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign({
+      id: user.id,
+      username: user.username,
+      email: user.email
+    }, JWT_SECRET);
   }
 }
