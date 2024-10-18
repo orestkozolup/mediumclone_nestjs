0a39dd850441a907f984b0b074f16a3132034d6a
Orest Kozolup <o.kozolup.extern@instamotion.com>
2024-10-18 14:06:50 +0200

    Add user to DB

---
+++ src/user/dto/createUser.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  readonly email: string;
+  readonly bio: string;
+  readonly image: string;
+  readonly password: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/createUser.dto";
+import { UserEntity } from "./user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UserService {
-  async createUser() {
-    return 'createUser'
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
+    return await this.userRepository.save(newUser);
   }
 }
